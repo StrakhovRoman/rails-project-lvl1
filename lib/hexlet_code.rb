1e1c3a06d299c8609c3291a1bbd50385f672ec09
Roman Strakhov <rvstrakhov@gmail.com>
2022-04-02 22:11:49 +0300

    update hexlet_code.rb

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,8 +2,15 @@
 
 require_relative "hexlet_code/version"
 require_relative "hexlet_code/tag"
+require_relative "hexlet_code/fields"
 
+# Main module
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.form_for(user, url: "#")
+    fields = HexletCode::Fields.new(user)
+    yield fields
+    Tag.build("form", action: url, method: "post") { fields.convert }
+  end
 end
