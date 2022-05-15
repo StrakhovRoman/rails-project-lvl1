33e1a3a2ac1a9535dc8b8febf39eebac2bc8d0e8
Roman Strakhov <rvstrakhov@gmail.com>
2022-05-15 15:11:02 +0300

    rename fields variable

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -8,9 +8,9 @@ require_relative 'hexlet_code/form_render'
 module HexletCode
   def self.form_for(entity, **options)
     url = options[:url] || '#'
-    fields = FormBuilder.new(entity)
-    yield fields
-    html = FormRender.render(fields.fields)
+    form_builder = FormBuilder.new(entity)
+    yield form_builder
+    html = FormRender.render(form_builder.fields)
     Tag.build('form', action: url, method: 'post') { html }
   end
 end
